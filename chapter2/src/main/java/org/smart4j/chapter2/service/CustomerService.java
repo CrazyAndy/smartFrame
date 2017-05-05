f6e49897b6a2fddaebd83e93b9f2583dc8c30ca8
CrazyAndy <auden.sh@163.com>
2017-05-05 16:37:51 +0800

    smartFrame练习2

---
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -0,0 +1,37 @@
+package org.smart4j.chapter2.service;
+
+import org.smart4j.chapter2.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by CrazyAndy
+ * Date: 2017/5/5
+ * Time: 16:18
+ */
+public class CustomerService {
+
+    public List<Customer> getCustomerList(String keyword){
+        //TODO
+        return null;
+    }
+
+    public Customer getCustomer(long id){
+        //TODO
+        return null;
+    }
+    public boolean createCustomer(Map<String,Object> fieldMap){
+        //TODO
+        return false;
+    }
+    public boolean updateCustomer(long id,Map<String,Object> fieldMap){
+        // TODO: 2017/5/5
+        return false;
+    }
+    public boolean deleteCustomer(long id){
+        // TODO: 2017/5/5
+        return false;
+    }
+
+}
---
+++ chapter2/src/main/webapp/WEB-INF/view/customer_create.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tiger
+  Date: 2017/5/5
+  Time: 16:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>创建客户界面</title>
+</head>
+<body>
+<h1>创建客户界面</h1>
+</body>
+</html>
