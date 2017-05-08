67f6d4f8b97f3496bceba979276cef7a18f87087
CrazyAndy <auden.sh@163.com>
2017-05-08 09:23:02 +0800

    搭建web框架

---
+++ smart-framework/src/main/java/org/smart4j/framework/bean/Request.java
@@ -0,0 +1,45 @@
+package org.smart4j.framework.bean;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ * Created by CrazyAndy
+ * Date: 2017/5/7
+ * Time: 11:44
+ */
+public class Request {
+    private String requestMethod;
+    private String requestPath;
+
+    public Request(String requestMethod, String requestPath) {
+        this.requestMethod = requestMethod;
+        this.requestPath = requestPath;
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public void setRequestMethod(String requestMethod) {
+        this.requestMethod = requestMethod;
+    }
+
+    public String getRequestPath() {
+        return requestPath;
+    }
+
+    public void setRequestPath(String requestPath) {
+        this.requestPath = requestPath;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this,obj);
+    }
+}
--- smart-framework/src/main/java/org/smart4j/framework/helper/IocHelper.java
+++ smart-framework/src/main/java/org/smart4j/framework/helper/IocHelper.java
@@ -1,10 +1,38 @@
 package org.smart4j.framework.helper;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.smart4j.framework.annotation.Inject;
+import org.smart4j.framework.util.CollectionUtil;
+import org.smart4j.framework.util.ReflectionUtil;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
 /**
  * Created by CrazyAndy
  * Date: 2017/5/7
  * Time: 9:24
  */
 public final class IocHelper {
-
+    static {
+        Map<Class<?>,Object> beanMap = BeanHelper.getBeanMap();
+        if(CollectionUtil.isNotEmpty(beanMap)){
+            for (Map.Entry<Class<?>,Object> beanEntry:beanMap.entrySet()) {
+                Class<?> beanClass = beanEntry.getKey();
+                Object beanInstance = beanEntry.getValue();
+                Field[] beanFields = beanClass.getDeclaredFields();
+                if(ArrayUtils.isNotEmpty(beanFields)){
+                    for(Field beanField:beanFields){
+                        if (beanField.isAnnotationPresent(Inject.class)){
+                            Class<?> beanFieldClass = beanField.getType();
+                            Object beanFieldInstance = beanMap.get(beanFieldClass);
+                            if(beanFieldInstance != null){
+                                ReflectionUtil.setField(beanInstance,beanField,beanFieldInstance);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
